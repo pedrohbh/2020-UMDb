8660c79b42da86e655e0e3f8cfc039486ade9b18
Breno Krohling <breno.aguiar@infobase.com.br>
2021-02-25 21:05:31 -0300

    Base do projeto frameweb com springboot

---
+++ umdb/src/main/java/com/ufes/inf/dwws/umdb/UmdbApplication.java
@@ -0,0 +1,13 @@
+package com.ufes.inf.dwws.umdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmdbApplication.class, args);
+	}
+
+}
