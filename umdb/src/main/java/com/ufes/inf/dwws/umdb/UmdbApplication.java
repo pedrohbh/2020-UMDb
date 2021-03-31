c39fce62340c446a1e6aa7ffaef71c945450053e
brenokrohling <brenokrohling@gmail.com>
2021-03-31 17:24:57 -0300

    Revert "Feature umdb 11"

--- umdb/src/main/java/com/ufes/inf/dwws/umdb/UmdbApplication.java
+++ umdb/src/main/java/com/ufes/inf/dwws/umdb/UmdbApplication.java
@@ -2,9 +2,8 @@ package com.ufes.inf.dwws.umdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class UmdbApplication {
 
 	public static void main(String[] args) {
