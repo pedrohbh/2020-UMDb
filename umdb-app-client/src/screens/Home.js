0ff225d226495d76d3d34f8bbdf991cb43c50a6f
Lucas Silva <lukasfabio@hotmail.com>
2021-04-16 00:15:07 -0300

    umdb-feature-7: Tela e rota MovieDetail criada

--- umdb-app-client/src/screens/Home.js
+++ umdb-app-client/src/screens/Home.js
@@ -25,16 +25,7 @@ const Home = () => {
                                 title='Rocky Balboa'
                                 year="2006"
                                 imageSource={rockyImage}
-                                link="/"
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/"
+                                link="/movie/1"
                                 style={{ marginBottom: '15px' }}
                             />
                         </Grid.Column>
@@ -44,7 +35,7 @@ const Home = () => {
                                 title='Rocky Balboa'
                                 year="2006"
                                 imageSource={rockyImage}
-                                link="/"
+                                link="/movie/2"
                                 style={{ marginBottom: '15px' }}
                             />
                         </Grid.Column>
@@ -54,7 +45,7 @@ const Home = () => {
                                 title='Rocky Balboa'
                                 year="2006"
                                 imageSource={rockyImage}
-                                link="/"
+                                link="/movie/3"
                                 style={{ marginBottom: '15px' }}
                             />
                         </Grid.Column>
@@ -64,7 +55,7 @@ const Home = () => {
                                 title='Rocky Balboa'
                                 year="2006"
                                 imageSource={rockyImage}
-                                link="/"
+                                link="/movie/4"
                                 style={{ marginBottom: '15px' }}
                             />
                         </Grid.Column>
@@ -74,7 +65,17 @@ const Home = () => {
                                 title='Rocky Balboa'
                                 year="2006"
                                 imageSource={rockyImage}
-                                link="/"
+                                link="/movie/5"
+                                style={{ marginBottom: '15px' }}
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={4}>
+                            <MovieCard
+                                fluid
+                                title='Rocky Balboa'
+                                year="2006"
+                                imageSource={rockyImage}
+                                link="/movie/6"
                                 style={{ marginBottom: '15px' }}
                             />
                         </Grid.Column>
