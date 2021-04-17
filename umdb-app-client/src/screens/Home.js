54c91a070fa0f6c86ce0782d88168193f05db6ac
Breno Krohling <breno.aguiar@infobase.com.br>
2021-04-17 02:04:03 -0300

    umdb-feature-22: integrando home com api

--- umdb-app-client/src/screens/Home.js
+++ umdb-app-client/src/screens/Home.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 
 import HomeContainer from '../components/HomeContainer';
 import MovieCard from '../components/MovieCard';
-import rockyImage from '../assets/rocky.jpeg'
+import { fetchMovies } from "../actions/movie";
+import {connect} from "react-redux";
 
 
-const Home = () => {
-    return (
+class Home extends Component{
+    constructor(props) {
+        super(props)
+
+    }
+
+    componentDidMount(){
+        this.props.fetchMovies();
+    }
+
+    renderRow(){
+        return this.props.movies.map((movie) =>{
+            return(
+                <Grid.Column width={4}>
+                    <MovieCard
+                        fluid
+                        title={movie.name}
+                        year={movie.year}
+                        imageSource={`data:image/gif;Base64,${movie.image}`}
+                        link={`/movie/${movie.id}`}
+                        avgRate={movie.avgRate}
+                        style={{ marginBottom: '15px' }}
+                    />
+                </Grid.Column>
+            )
+        })
+    }
+
+    render(){
+        return (
         <>
             <Grid>
                 <Grid.Row>
@@ -19,71 +48,29 @@ const Home = () => {
             <HomeContainer>
                 <Grid>
                     <Grid.Row>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/movie/1"
-                                style={{ marginBottom: '15px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/movie/2"
-                                style={{ marginBottom: '15px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/movie/3"
-                                style={{ marginBottom: '15px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/movie/4"
-                                style={{ marginBottom: '15px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/movie/5"
-                                style={{ marginBottom: '15px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <MovieCard
-                                fluid
-                                title='Rocky Balboa'
-                                year="2006"
-                                imageSource={rockyImage}
-                                link="/movie/6"
-                                style={{ marginBottom: '15px' }}
-                            />
-                        </Grid.Column>
+                        {
+                            this.props.movies.length > 0 ? (
+                                this.renderRow()
+                            ): (
+                                <Grid.Column width={4}>
+                                    <div>Não há filmes cadastrados</div>
+                                </Grid.Column>
+                            )
+                        }
                     </Grid.Row>
                 </Grid>
             </HomeContainer>
         </>
     );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return { movies: Object.values(state.movies) };
 };
 
-export default Home;
+export default connect(
+    mapStateToProps,
+    { fetchMovies }
+)(Home);
