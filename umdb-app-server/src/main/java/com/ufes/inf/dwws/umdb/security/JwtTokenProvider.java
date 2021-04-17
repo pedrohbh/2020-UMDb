4d48a1d6e51d0f268d16a109fb375daec5316c8d
Breno Krohling <breno.aguiar@infobase.com.br>
2021-04-17 01:48:14 -0300

    fix bug delete movie e token

--- umdb-app-server/src/main/java/com/ufes/inf/dwws/umdb/security/JwtTokenProvider.java
+++ umdb-app-server/src/main/java/com/ufes/inf/dwws/umdb/security/JwtTokenProvider.java
@@ -54,8 +54,8 @@ public class JwtTokenProvider {
     }
     public String resolveToken(HttpServletRequest req) {
         String bearerToken = req.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer Token")) {
-            return bearerToken.substring(13, bearerToken.length());
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
         }
         return null;
     }
